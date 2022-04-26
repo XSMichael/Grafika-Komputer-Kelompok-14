d003c0aa53d29df167083f63ca574d28d21981ae
XSMichael <104413706+XSMichael@users.noreply.github.com>
2022-04-26 14:25:36 +0800

    Add files via upload

---
+++ Digimon/Viximon.cs
@@ -0,0 +1,264 @@
+﻿using LearnOpenTK.Common;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digimon
+{
+    internal class Viximon : MyObject
+    {
+        Assets tail;
+        Assets ear1;
+        Assets ear2;
+        private Assets mulut;
+        private int counter = 0;
+
+        public Viximon()
+        {
+            setDefault();
+        }
+
+        public Viximon(Vector3 centerPosition, bool status = true)
+        {
+            setDefault();
+            this._centerPosition = centerPosition;
+            this.status = status;
+        }
+
+        public override void setDefault()
+        {
+            base.setDefault();
+            radius_x = (float)160 / 400;
+            radius_y = radius_x;
+            radius_z = radius_x;
+        }
+
+        public override void load(string shaderVert, string shaderFrag, float Size_x, float Size_y)
+        {
+            base.load(shaderVert, shaderFrag, Size_x, Size_y);
+            Assets temp_object;
+            //isi assets
+
+            parentObj = new Assets(1, new Vector3(255, 230, 255));
+            temp_object = new Assets(1, new Vector3(255, 230, 0));
+            temp_object.createEllipsoid(0.0f, 0.0f, 0.0f, 0.4f, 0.4f, 0.5f);
+            parentObj.addChild(temp_object);
+
+            #region ekor
+            //ekor
+            tail = new Assets(1, new Vector3(255, 230, 0));
+            tail.createEllipsoid(0.0f, 0.2f, -0.5f, 0.5f, 0.1f, 0.1f);
+            tail.rotate(tail.getCenter(), tail._euler[2], 90f);
+            tail.rotate(tail.getCenter(), tail._euler[1], 60f);
+            tail.setCenter(0, -radius_y * 38 / 40, -radius_z);
+            parentObj.addChild(tail);
+
+            temp_object = new Assets(1, new Vector3(255, 255, 255));
+            temp_object.createEllipticParaboloid(0.0f, 0.52f, -1.05f, 0.12f, 0.12f, 0.6f);
+            temp_object.rotate(temp_object.getCenter(), temp_object._euler[0], 30f);
+
+            tail.addChild(temp_object);
+            #endregion
+
+
+            #region mata
+            //Mata putih
+
+            temp_object = new Assets(1, new Vector3(255, 255, 255));
+            temp_object.createCylinder2(-0.15f, -0.05f, 0.33f, 0.1f, 0.2f, 0.3f);
+            temp_object.rotate(temp_object.getCenter(), temp_object._euler[1], 180);
+            parentObj.addChild(temp_object);
+
+            temp_object = new Assets(1, new Vector3(255, 255, 255));
+            temp_object.createCylinder2(0.15f, -0.05f, 0.33f, 0.1f, 0.2f, 0.3f);
+            temp_object.rotate(temp_object.getCenter(), temp_object._euler[1], 180);
+            parentObj.addChild(temp_object);
+
+            //pupils
+            temp_object = new Assets(1, new Vector3(51, 153, 255));
+            temp_object.createCylinder2(-0.13f, -0.05f, 0.48f, 0.07f, 0.15f, 0.05f);
+            temp_object.rotate(temp_object.getCenter(), temp_object._euler[1], 180);
+            parentObj.addChild(temp_object);
+
+            temp_object = new Assets(1, new Vector3(51, 153, 255));
+            temp_object.createCylinder2(0.13f, -0.05f, 0.48f, 0.07f, 0.15f, 0.05f);
+            temp_object.rotate(temp_object.getCenter(), temp_object._euler[1], 180);
+            parentObj.addChild(temp_object);
+
+            //alis kiri
+            Assets alisKiri = new Assets(2, new Vector3(0, 0, 0));
+            alisKiri.setVertices(new List<Vector3>());
+            alisKiri.addVertices(0.0f, 0.02f, 1.0f);
+            alisKiri.addVertices(0.05f, 0.05f, 1.0f);
+            alisKiri.addVertices(0.10f, 0.0f, 1.0f);
+            alisKiri.setVertices(alisKiri.createCurveBezier());
+            alisKiri.Translation(new Vector3(0.1f, 0.22f, -0.57f));
+            parentObj.addChild(alisKiri);
+
+            //alis kanan
+            Assets alisKanan = new Assets(2, new Vector3(0, 0, 0));
+            alisKanan.setVertices(new List<Vector3>());
+            alisKanan.addVertices(0.0f, 0.02f, 1.0f);
+            alisKanan.addVertices(-0.05f, 0.05f, 1.0f);
+            alisKanan.addVertices(-0.10f, 0.0f, 1.0f);
+            alisKanan.setVertices(alisKanan.createCurveBezier());
+            alisKanan.Translation(new Vector3(-0.1f, 0.22f, -0.57f));
+            parentObj.addChild(alisKanan);
+
+            //Eye details
+            temp_object = new Assets(1, new Vector3(0, 0, 0));
+            temp_object.createEllipsoid(0.13f, 0.027f, 0.5f, 0.04f, 0.05f, 0.02f);
+            parentObj.addChild(temp_object);
+
+            temp_object = new Assets(1, new Vector3(255, 255, 255));
+            temp_object.createEllipsoid(0.16f, 0.08f, 0.5f, 0.025f, 0.025f, 0.01f);
+            parentObj.addChild(temp_object);
+
+            temp_object = new Assets(1, new Vector3(255, 255, 255));
+            temp_object.createEllipsoid(0.09f, -0.01f, 0.52f, 0.015f, 0.015f, 0.01f);
+            parentObj.addChild(temp_object);
+
+            temp_object = new Assets(1, new Vector3(0, 0, 0));
+            temp_object.createEllipsoid(-0.13f, 0.027f, 0.5f, 0.04f, 0.05f, 0.02f);
+            parentObj.addChild(temp_object);
+
+            temp_object = new Assets(1, new Vector3(255, 255, 255));
+            temp_object.createEllipsoid(-0.16f, 0.08f, 0.5f, 0.025f, 0.025f, 0.01f);
+            parentObj.addChild(temp_object);
+
+            temp_object = new Assets(1, new Vector3(255, 255, 255));
+            temp_object.createEllipsoid(-0.09f, -0.01f, 0.52f, 0.015f, 0.015f, 0.01f);
+            parentObj.addChild(temp_object);
+            #endregion
+
+            #region telinga
+            //Telinga 
+            ear1 = new Assets(1, new Vector3(255, 230, 0));
+            ear1.createEllipticParaboloid(-0.3f, 0.65f, 0.2f, 0.5f / 4, 0.2f / 4, 0.5f * 4 / 3);
+            parentObj.addChild(ear1);
+
+            temp_object = new Assets(1, new Vector3(255, 255, 255));
+            temp_object.createEllipticParaboloid(-0.3f, 0.65f, 0.2f, 0.55f / 4, 0.25f / 4, 0.4f * 4 / 3);
+
+            ear1.addChild(temp_object);
+
+            ear1.rotate(temp_object.getCenter(), temp_object._euler[0], 90);
+            ear1.rotate(temp_object.getCenter(), temp_object._euler[1], 15);
+
+
+            ear2 = new Assets(1, new Vector3(255, 230, 0));
+            ear2.createEllipticParaboloid(0.3f, 0.65f, 0.2f, 0.5f / 4, 0.2f / 4, 0.5f * 4 / 3);
+            parentObj.addChild(ear2);
+
+            temp_object = new Assets(1, new Vector3(255, 255, 255));
+            temp_object.createEllipticParaboloid(0.3f, 0.65f, 0.2f, 0.55f / 4, 0.25f / 4, 0.4f * 4 / 3);
+
+            ear2.addChild(temp_object);
+
+            ear2.rotate(temp_object.getCenter(), temp_object._euler[0], 90);
+            ear2.rotate(temp_object.getCenter(), temp_object._euler[1], -15);
+            #endregion
+
+            #region kaki
+            //kaki
+            temp_object = new Assets(1, new Vector3(255, 230, 0));
+            temp_object.createEllipsoid(-0.24f, -0.30f, 0.25f, 0.07f, 0.13f, 0.07f);
+            parentObj.addChild(temp_object);
+            temp_object.rotate(temp_object.getCenter(), temp_object._euler[2], -20);
+
+            temp_object = new Assets(1, new Vector3(255, 230, 0));
+            temp_object.createEllipsoid(-0.24f, -0.30f, -0.25f, 0.07f, 0.13f, 0.07f);
+            parentObj.addChild(temp_object);
+            temp_object.rotate(temp_object.getCenter(), temp_object._euler[2], -20);
+
+            temp_object = new Assets(1, new Vector3(255, 230, 0));
+            temp_object.createEllipsoid(0.24f, -0.30f, 0.25f, 0.07f, 0.13f, 0.07f);
+            parentObj.addChild(temp_object);
+            temp_object.rotate(temp_object.getCenter(), temp_object._euler[2], 20);
+
+            temp_object = new Assets(1, new Vector3(255, 230, 0));
+            temp_object.createEllipsoid(0.24f, -0.30f, -0.25f, 0.07f, 0.13f, 0.07f);
+            parentObj.addChild(temp_object);
+            temp_object.rotate(temp_object.getCenter(), temp_object._euler[2], 20);
+
+            #endregion
+
+            #region uwu mouth
+            //UWU mouth
+            mulut = new Assets(2, new Vector3(0, 0, 0));
+            mulut.setVertices(new List<Vector3>());
+            mulut.addVertices(0.0f, 0.03f, 1.0f);
+            mulut.addVertices(0.03f, 0.0f, 1.0f);
+            mulut.addVertices(0.06f, 0.03f, 1.0f);
+            List<Vector3> mulutKiri = new List<Vector3>();
+            mulutKiri = mulut.createCurveBezier();
+
+            mulut.setVertices(new List<Vector3>());
+            mulut.addVertices(0.06f, 0.03f, 1.0f);
+            mulut.addVertices(0.09f, 0.0f, 1.0f);
+            mulut.addVertices(0.12f, 0.03f, 1.0f);
+            List<Vector3> mulutKanan = new List<Vector3>();
+            mulutKanan = mulut.createCurveBezier();
+            mulutKiri.AddRange(mulutKanan);
+
+            mulut.setVertices(mulutKiri);
+            mulut.Translation(new Vector3(-0.06f, -0.15f, -0.51f));
+            parentObj.addChild(mulut);
+            #endregion
+
+            Assets temp = new Assets(2, new Vector3(0, 0, 0));
+            temp.setVertices(new List<Vector3>());
+            temp.addVertices(0.0f, 0.00f, 0f);
+            temp.addVertices(0.0f, 0.00f, 0f);
+            temp.addVertices(0.0f, 0.00f, 0f);
+            parentObj.addChild(temp);
+
+            Translation(-0.0f, -0.17f, 0.0f);
+            parentObj.Scaling(new Vector3(0.4f, 0.4f, 0.4f));
+            parentObj.load(shaderVert, shaderFrag, Size_x, Size_y);
+        }
+
+        public override void render(FrameEventArgs args, Matrix4 camera_view, Matrix4 camera_projection)
+        {
+            base.render(args, camera_view, camera_projection);
+
+            parentObj.render(camera_view, camera_projection);
+            idle();
+            idle2();
+        }
+
+        public void idle()
+        {
+            if (statusIdle1)
+            {
+                Vector3 tempCenter = tail.getCenter();
+                Vector3 body = parentObj.getCenter();
+                counter += 1;
+                if (counter <= 60)
+                {
+                    Scale(0.9965f, 1 / 0.9965f, 0.9965f);
+                    tail.rotate(tempCenter, tail._euler[1], 0.75f);
+                    ear1.rotate(body, ear1._euler[1], 0.3f);
+                    ear2.rotate(body, ear1._euler[1], -0.3f);
+                }
+                else if (counter <= 121)
+                {
+                    Scale(1 / 0.9965f, 0.9965f, 1 / 0.9965f);
+                    tail.rotate(tempCenter, tail._euler[1], -0.75f);
+                    ear1.rotate(body, ear1._euler[1], -0.3f);
+                    ear2.rotate(body, ear1._euler[1], 0.3f);
+                }
+                else
+                {
+                    counter = 0;
+                }
+            }
+        }
+
+    }
+}
---
+++ Digimon/Window.cs
@@ -0,0 +1,368 @@
+﻿using LearnOpenTK.Common;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Mathematics;
+
+
+namespace Digimon
+{
+    
+    static class Constants
+    {
+        public const string path = "../../../Shaders/";
+
+    }
+    internal class Window : GameWindow
+    {
+        List<MyObject> _object = new List<MyObject>();
+        Camera _camera;
+        bool _firstMove = true;
+        Vector2 _lastPos;
+        Vector3 _objectPos = new Vector3(0, 0, 0);
+        float _rotationSpeed = 1f;
+
+        double _time = 0;
+        double rotate_speed = 100;
+        bool isRotate = false; //rotate seluruh object di 1 pusat
+        int typeTransform = 1; //untuk rotate dan translate per object
+
+        public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+        {
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            GL.ClearColor(51/255f, 231 / 255f, 255 / 255f, 1.0f);
+            GL.Enable(EnableCap.DepthTest);
+            GL.Enable(EnableCap.Blend);
+
+
+            _object = new List<MyObject>();
+            _time = 0;
+            rotate_speed = 100;
+            isRotate = false;
+            typeTransform = 1;
+
+            Viximon viximon = new Viximon();
+            _object.Add(viximon);
+
+            Budmon budMonObj = new Budmon();
+            _object.Add(budMonObj);
+            
+            Kapurimon kapurimon = new Kapurimon();
+            _object.Add(kapurimon);
+
+            House house = new House();
+            _object.Add(house);
+
+            Ground ground = new Ground();
+            _object.Add(ground);
+
+            Sky sky = new Sky();
+            _object.Add(sky);
+            Atmosphere atmosphere = new Atmosphere();
+            _object.Add(atmosphere);
+
+            
+
+            for (int i = 0; i < _object.Count(); i++)
+            {
+                _object[i].load(Constants.path + "Shader.vert", Constants.path + "shader.frag", Size.X, Size.Y);
+            }
+
+            #region Camera
+            _camera = new Camera(new Vector3(0, 0, 2.5f), Size.X / Size.Y);
+            #endregion
+
+            CursorGrabbed = true;
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+            _time = rotate_speed * args.Time;
+
+
+            _object[0].render(args, _camera.GetViewMatrix(), _camera.GetProjectionMatrix());
+            _object[1].render(args, _camera.GetViewMatrix(), _camera.GetProjectionMatrix());
+            _object[2].render(args, _camera.GetViewMatrix(), _camera.GetProjectionMatrix());
+            _object[3].render(args, _camera.GetViewMatrix(), _camera.GetProjectionMatrix());
+            _object[4].render(args, _camera.GetViewMatrix(), _camera.GetProjectionMatrix());
+            _object[5].render(args, _camera.GetViewMatrix(), _camera.GetProjectionMatrix());
+            _object[6].render(args, _camera.GetViewMatrix(), _camera.GetProjectionMatrix());
+
+            SwapBuffers();
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0, 0, Size.X, Size.Y);
+            _camera.AspectRatio = Size.X / (float)Size.Y;
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+            //biasa untuk mengecek apakah ada terima inputan dri keyboard atau tidak
+
+            if (!IsFocused)
+            {
+                return; //Reject semua input saat window bukan focus.
+            }
+
+            var input = KeyboardState;
+            if (input.IsKeyDown(Keys.Escape))//waktu tombol sedang ditekan
+            {
+                Close();
+            }
+            
+            #region camera
+            float cameraSpeed = 0.5f;
+            if (input.IsKeyDown(Keys.W))
+            {
+                _camera.Position += _camera.Front * cameraSpeed * (float)args.Time;
+            }
+            if (input.IsKeyDown(Keys.S))
+            {
+                _camera.Position -= _camera.Front * cameraSpeed * (float)args.Time;
+            }
+            if (input.IsKeyDown(Keys.A))
+            {
+                _camera.Position -= _camera.Right * cameraSpeed * (float)args.Time;
+            }
+            if (input.IsKeyDown(Keys.D))
+            {
+                _camera.Position += _camera.Right * cameraSpeed * (float)args.Time;
+            }
+            if (input.IsKeyDown(Keys.Space))
+            {
+                _camera.Position += _camera.Up * cameraSpeed * (float)args.Time;
+            }
+            if (input.IsKeyDown(Keys.LeftShift))
+            {
+                _camera.Position -= _camera.Up * cameraSpeed * (float)args.Time;
+            }
+
+            var mouse = MouseState;
+            var sensitivity = 0.1f;
+
+            if (_firstMove)
+            {
+                _lastPos = new Vector2(mouse.X, mouse.Y);
+                _firstMove = false;
+            }
+            else
+            {
+                var deltaX = mouse.X - _lastPos.X;
+                var deltaY = mouse.Y - _lastPos.Y;
+                _lastPos = new Vector2(mouse.X, mouse.Y);
+                _camera.Yaw += deltaX * sensitivity; //rotate kiri kanan
+                _camera.Pitch -= deltaY * sensitivity; //rotate atas bawah //ada batasan jauh rotate
+            }
+
+            #endregion
+
+            //reset button
+            if (input.IsKeyPressed(Keys.RightShift))
+            {
+                this.OnLoad();
+            }
+
+            // on/off idle1
+            if (input.IsKeyPressed(Keys.RightControl))
+            {
+                bool temp = !_object[0].isIdle1();
+                foreach(var obj in _object)
+                {
+                    obj.isIdle1(temp);
+                }
+            }
+            // on/off all idle
+            if (input.IsKeyPressed(Keys.RightAlt))
+            {
+                bool temp = !_object[0].isIdle2();
+                foreach (var obj in _object)
+                {
+                    obj.isIdle1(temp);
+                    obj.isIdle2(temp);
+                }
+            }
+
+            #region transformasi
+            //enable/disable object for using rotate, translation
+            if (input.IsKeyPressed(Keys.D1))
+            {
+                _object[0].changeStatus();
+                isRotate = false;
+            }
+            else if (input.IsKeyReleased(Keys.D2))
+            {
+                _object[1].changeStatus();
+                isRotate = false;
+            }
+            else if (input.IsKeyReleased(Keys.D3))
+            {
+                _object[2].changeStatus();
+                isRotate = false;
+            }
+
+            //Mengaktifkan salah satu dari rotate, translation
+            if (input.IsKeyPressed(Keys.LeftControl))
+            {
+                isRotate = !isRotate;
+                if (isRotate)
+                {
+                    foreach(var obj in _object)
+                    {
+                        obj.setStatus(false);
+                    }
+                }
+                //isRotate dan isTranslate selalu berlawanan
+            }else if (input.IsKeyPressed(Keys.LeftAlt))
+            {
+                isRotate = false;
+                if(typeTransform == 0)
+                {
+                    typeTransform = 1;
+                }else if(typeTransform == 1)
+                {
+                    typeTransform = 0;
+                }
+            }
+
+            //*Rotate: left right sumbu y
+            //*Translation: up down sumbu z, left right x
+            //*Rotate keseluruhan: left right sumbu y
+
+            if (input.IsKeyDown(Keys.Up))
+            {
+                foreach(var obj in _object)
+                {
+                    if (obj.getStatus())
+                    {
+                        if (typeTransform == 0)
+                        {
+                            obj.Translation(0, 0, -0.008f);
+                        }
+                    }
+                }
+            }else if (input.IsKeyDown(Keys.Down))
+            {
+                foreach (var obj in _object)
+                {
+                    if (obj.getStatus())
+                    {
+                        if (typeTransform == 0)
+                        {
+                            obj.Translation(0, 0, 0.008f);
+                        }
+                    }
+                }
+            }
+            else if (input.IsKeyDown(Keys.Right))
+            {
+                if (!isRotate)
+                {
+                    foreach (var obj in _object)
+                    {
+                        if (obj.getStatus())
+                        {
+                            if (typeTransform == 0)
+                            {
+                                obj.Translation(0.008f, 0, 0);
+                            }
+                            else if (typeTransform == 1)
+                            {
+                                obj.resetScale();
+                                obj.Rotate(obj.getCenter(),1, _time);
+                                obj.addRotateValue(new Vector3(0, (float)_time, 0));
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    foreach(var obj in _object)
+                    {
+                        obj.resetScale();
+                        obj.Rotate(new Vector3(0, 0, 0), 1, _time);
+                    }
+                }
+                
+            }
+            else if (input.IsKeyDown(Keys.Left))
+            {
+                if (!isRotate)
+                {
+                    foreach (var obj in _object)
+                    {
+                        if (obj.getStatus())
+                        {
+                            if (typeTransform == 0)
+                            {
+                                obj.Translation(-0.008f, 0,0);
+                            }
+                            else if (typeTransform == 1)
+                            {
+                                obj.resetScale();
+                                obj.Rotate(obj.getCenter(),1, -_time);
+                                obj.addRotateValue(new Vector3(0, (float)-_time, 0));
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var obj in _object)
+                    {
+                        obj.resetScale();
+                        obj.Rotate(new Vector3(0, 0, 0), 1, -_time);
+                    }
+                }
+            }
+            #endregion
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+        }
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            base.OnMouseWheel(e);
+            _camera.Fov = _camera.Fov - e.OffsetY;
+        }
+
+        public Matrix4 generateArbRotationMatrix(Vector3 axis, Vector3 center, float degree)
+        {
+            var rads = MathHelper.DegreesToRadians(degree);
+
+            var secretFormula = new float[4, 4] {
+                { (float)Math.Cos(rads) + (float)Math.Pow(axis.X, 2) * (1 - (float)Math.Cos(rads)), axis.X* axis.Y * (1 - (float)Math.Cos(rads)) - axis.Z * (float)Math.Sin(rads),    axis.X * axis.Z * (1 - (float)Math.Cos(rads)) + axis.Y * (float)Math.Sin(rads),   0 },
+                { axis.Y * axis.X * (1 - (float)Math.Cos(rads)) + axis.Z * (float)Math.Sin(rads),   (float)Math.Cos(rads) + (float)Math.Pow(axis.Y, 2) * (1 - (float)Math.Cos(rads)), axis.Y * axis.Z * (1 - (float)Math.Cos(rads)) - axis.X * (float)Math.Sin(rads),   0 },
+                { axis.Z * axis.X * (1 - (float)Math.Cos(rads)) - axis.Y * (float)Math.Sin(rads),   axis.Z * axis.Y * (1 - (float)Math.Cos(rads)) + axis.X * (float)Math.Sin(rads),   (float)Math.Cos(rads) + (float)Math.Pow(axis.Z, 2) * (1 - (float)Math.Cos(rads)), 0 },
+                { 0, 0, 0, 1}
+            };
+            var secretFormulaMatix = new Matrix4
+            (
+                new Vector4(secretFormula[0, 0], secretFormula[0, 1], secretFormula[0, 2], secretFormula[0, 3]),
+                new Vector4(secretFormula[1, 0], secretFormula[1, 1], secretFormula[1, 2], secretFormula[1, 3]),
+                new Vector4(secretFormula[2, 0], secretFormula[2, 1], secretFormula[2, 2], secretFormula[2, 3]),
+                new Vector4(secretFormula[3, 0], secretFormula[3, 1], secretFormula[3, 2], secretFormula[3, 3])
+            );
+
+            return secretFormulaMatix;
+        }
+    }
+}
